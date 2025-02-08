3fdeabd1b3ad5869174668e6f205185174b6d772
YieldRay <yieldray@outlook.com>
2025-02-08 22:03:45 +0800

    port

--- main.ts
+++ main.ts
@@ -6,14 +6,15 @@ import { type WebdOptions } from "./webd.ts";
 
 const url = process.env["LIBSQL_URL"] || "file:local.db";
 const authToken = process.env["AUTH_TOKEN"];
+const port = Number(process.env["PORT"] || 3000);
 
 const sqliteFs = new SqliteFs(new LibsqlDialect({ url, authToken }));
-const options: WebdOptions = { browser: "list" };
+const options: WebdOptions = { browser: process.env["WEBD_BROWSER"] as any };
 
 if (typeof Deno === "object") {
     const handler = createServeHandler(sqliteFs, options);
-    Deno.serve(handler);
+    Deno.serve({ handler, port });
 } else {
-    console.log("Listening on http://localhost:8000");
-    createNodeServer(sqliteFs, options).listen(8000);
+    console.log(`Listening on http://localhost:${port}`);
+    createNodeServer(sqliteFs, options).listen(port);
 }
